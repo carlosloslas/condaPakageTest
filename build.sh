76221ed826a4a0e0cc3128bc9a57910e92d5d738
closada <losadalastra.carlos@gmail.com>
2017-07-14 22:01:03 +0100

    Added License, meta, & build.sh scripts

---
+++ build.sh
@@ -0,0 +1,7 @@
+# -------------------------------------------------------------------------- #
+#                                  build.sh                                  #
+# -------------------------------------------------------------------------- #
+# Shell script for Linux and OS X. Calls the build scrip of the package.
+# -------------------------------------------------------------------------- #
+
+$PYTHON setup.py install     # Python command to install the script.
