d69c6eb580f3ca135fb3a13a2597c1da1aff93a7
closada <losadalastra.carlos@gmail.com>
2017-07-14 22:38:26 +0100

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='condaPackageTest',
-      version='0.1',
-      description='testing how to make an anaconda package',
-      author='Carlos Losada',
-      author_email='losadalastra.carlos@gmail.com',
-      url='https://github.com/carlosloslas/condaPakageTest',
-      packages=['condaPackageTest'],
+setup(
+    name="condaPackageTest",
+    packages=['condaPackageTest'],
+    version="0.1",
+    description="testing how to make an anaconda package",
+    author="Carlos Losada",
+    author_email="losadalastra.carlos@gmail.com",
+    url="https://github.com/carlosloslas/condaPakageTest",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
      )
