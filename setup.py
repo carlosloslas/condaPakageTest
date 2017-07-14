b7be3fc899bbf5104a5e5036fee7d31b3e70794f
closada <losadalastra.carlos@gmail.com>
2017-07-14 22:18:52 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='condaPackageTest',
+      version='0.1',
+      description='testing how to make an anaconda package',
+      author='Carlos Losada',
+      author_email='losadalastra.carlos@gmail.com',
+      url='https://github.com/carlosloslas/condaPakageTest',
+      packages=['condaPackageTest'],
+     )
