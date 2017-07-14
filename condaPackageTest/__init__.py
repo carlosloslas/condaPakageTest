0e676c660a169d277a16f1c13067d8376e30ab53
closada <losadalastra.carlos@gmail.com>
2017-07-14 22:41:35 +0100

    adding main function to __init__.py file

--- condaPackageTest/__init__.py
+++ condaPackageTest/__init__.py
@@ -1,1 +1,5 @@
-print("Hello World from my package test @__init__.py!")
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+    print("Hello World from my package test @__init__.py!")
